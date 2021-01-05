76fb38f54edeecf7bf428669b50f776fd82c4d9b
Hyperion <60348895+Light134@users.noreply.github.com>
2021-01-05 20:06:56 +0900

    Update calamity.js

--- scripts/blocks/calamity.js
+++ scripts/blocks/calamity.js
@@ -7,15 +7,20 @@ extendContent(ItemTurret.ItemTurretBuild, calamity, {
 	heatcap:0,
 	cooldown:true,
 	toggle:false,
+	t1 : 0,
+	t2 : 0,
 	updateTile(){
 		this.super$updateTile();
 		
+		this.t1 = Mathf.lerpDelta(this.t1, this.heatcap / 10, 0.05 );
+		this.t2 = Mathf.lerpDelta(this.t2, this.overheat / 300, 0.2 );
+	
 		this.overheat = Math.max(0, this.overheat -= Time.delta);
 		this.heatcap = Math.max(0, this.heatcap);
 		
 		if(this.overheat >= 301) {
 			this.toggle = true;
-			this.overheat = 240;
+			this.overheat = 301;
 			this.heatcap = 10;
 		}
 		else if(this.overheat == 0) {
@@ -23,14 +28,22 @@ extendContent(ItemTurret.ItemTurretBuild, calamity, {
 			this.cooldown = true;
 		} else this.cooldown = false;
 		
-		if(this.toggle && this.heatcap > 0) this.heatcap -= (Time.delta / 2);
+		if(this.toggle && this.heatcap > 0) this.heatcap -= (Time.delta / 30);
 		
-		if(!this.toggle && this.cooldown && this.heatcap > 0) this.heatcap -= (Time.delta / 30);
-		
-		print("overheat: " + this.overheat)
-		print("heatcap: " + this.heatcap)
+		if(!this.toggle && this.cooldown && this.heatcap > 0) this.heatcap -= (Time.delta / 60);
 		
 	},
+	drawSelect(){
+		Lines.stroke(this.t1 * 1);
+		Draw.color(Color.valueOf("8aa3f4"));
+		Lines.polySeg(200, 0, 200*this.t1, this.x, this.y, 1.5*8, 0);
+		Lines.stroke(this.t1 * 2);
+		Draw.color(Color.valueOf("ffa665"));
+		Lines.polySeg(200, 0, 200*this.t2, this.x, this.y, 1.5*8, 0);
+		
+		Draw.color();
+	},
+	
 	
 	shoot(type){
 		if(!this.toggle) {
@@ -47,6 +60,6 @@ extendContent(ItemTurret.ItemTurretBuild, calamity, {
 		this.super$updateShooting()
 	},
 	baseReloadSpeed(){
-		return this.efficiency() * (1 + (this.heatcap/5));
+		return this.efficiency() * (1 + (this.heatcap/10));
 	}
 });
