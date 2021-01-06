68cf845aedefef906613f94c2d69b1dacc42d244
Hyperion <60348895+Light134@users.noreply.github.com>
2021-01-06 15:05:03 +0900

    Update calamity.js

--- scripts/blocks/calamity.js
+++ scripts/blocks/calamity.js
@@ -34,6 +34,7 @@ extendContent(ItemTurret.ItemTurretBuild, calamity, {
 		
 	},
 	drawSelect(){
+		this.super$drawSelect();
 		Lines.stroke(this.t1 * 1);
 		Draw.color(Color.valueOf("8aa3f4"));
 		Lines.polySeg(200, 0, 200*this.t1, this.x, this.y, 1.5*8, 0);
